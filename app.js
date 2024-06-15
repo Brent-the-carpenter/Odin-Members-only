b941d61049ec11adca4b0d87fffcaa0bc8a1959c
Brent-the-carpenter <beh337@gmail.com>
2024-06-14 23:24:30 -0400

    I have fleshed out the signup form and added a header and footer to the layout. I have also added a main layout file to the views/layouts directory. I created the user model and added the signup controller to handle the signup form submission. added the message model and the message controller skeletons. Tomorrow I will add the message form and the message list to the layout. I will also add the message controller to handle the message form submission.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Package imports */
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -12,29 +13,34 @@ const debugDB = require("debug")("app:db");
 const passport = require("passport");
 const passportConfig = require("./config/passportConfig");
 
+/* Route imports */
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const session = require("express-session");
 
+/* Model imports */
+const User = require("./models/user");
+
 const Secret = process.env.SECRET;
 const MongoDB = process.env.MONGOURI;
 const Limiter = RateLimit({
-  windowMS: 1 * 60 * 60 * 1000,
-  max: 30,
+  windowMs: 1 * 60 * 60 * 1000,
+  max: 500,
 });
 
 const app = express();
 
-// set up Mongo
+// Set up Mongo
 mongoose.set("strictQuery", false);
 async function main() {
   await mongoose.connect(MongoDB);
-  debugDB("MongoDB: succesfully connected to DB");
+  debugDB("MongoDB: successfully connected to DB");
 }
 main().catch((err) => debugDB(err));
 
-// view engine setup
+// View engine setup
 app.use(expressEjsLayouts);
+app.set("layout", "./layouts/main");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(session({ secret: Secret, resave: false, saveUninitialized: true }));
@@ -55,31 +61,39 @@ passport.serializeUser((user, done) => {
 });
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await User.findByID(id);
+    const user = await User.findById(id);
     done(null, user);
   } catch (err) {
     done(err);
   }
 });
+
 app.use((req, res, next) => {
-  req.locals.currentUser = req.user;
+  res.locals.currentUser = req.user;
   next();
 });
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
+// Debugging middleware to log res.locals
+app.use((req, res, next) => {
+  console.log("res.locals:", res.locals);
+  next();
+});
+
+app.use("/", indexRouter);
+app.use("/user", usersRouter);
+
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render("error");
 });
--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,10 +1,61 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+const debugSignUp = require("debug")("app:signUp");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
 const options = { title: "SignUp" };
 exports.get_sign_up = asyncHandler(async (req, res, next) => {
-  res.render("signUp", options);
+  res.render("signup_form", options);
 });
 
-exports.post_sign_up = asyncHandler(async (req, res, next) => {
-  body();
-});
+exports.post_sign_up = [
+  body("first_name", " First name is required.")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Must be at least one character long.")
+    .isAlpha()
+    .withMessage("Must contain only Alpha characters."),
+  body("last_name", "Last name is required.")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Must be at least one character long."),
+  body("username", "Username is required.").trim().escape(),
+  body("password", "Password is required.").trim().escape().isStrongPassword(),
+  body("password_confirm", "Please confirm your password.").trim().escape(),
+  asyncHandler(async (req, res, next) => {
+    try {
+      const isUser = await User.find({ username: req.body.username }).exec();
+      if (isUser) {
+        options.userTaken = "A account with this username already exsist.";
+        return res.render("signup_form", options);
+      }
+      if (req.body.password !== req.body.password_confirm) {
+        options.password_match = false;
+        return res.render("signup_form", options);
+      }
+      bcrypt.hash(req.body.password, 10, async (err, hashedpassword) => {
+        if (err) {
+          const err = new Error("Error hashing password");
+          err.status = 500;
+          err.stack = err;
+          throw err;
+        }
+        const user = new User({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          username: req.body.username,
+          password: hashedpassword,
+        });
+        debugSignUp(`saved user: ${user}`);
+        await user.save();
+        res.redirect("/");
+      });
+    } catch (error) {
+      debugSignUp("debugSignUp: error signing up error:", error);
+      return next(error);
+    }
+  }),
+];
